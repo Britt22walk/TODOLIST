4f584b941939dee678399b5f863b401c00a268ed
Brittany Walker <bwalker005@gmail.com>
2021-06-02 19:14:23 -0700

    add button works

--- src/components/AddItem/AddItem.jsx
+++ src/components/AddItem/AddItem.jsx
@@ -3,14 +3,14 @@ import React, { Component } from 'react';
 class AddItem extends Component {
   onSubmitForm = (e) => {
     e.preventDefault()
-    this.props.updateListItems(e.target.value)
+    this.props.updateListItems(e.target.newItem.value)
   }
     render() { 
         return (
         <div id="add-form" className="add-form m-1 p-1">
             <form onSubmit={this.onSubmitForm}>
                 <label>Add Item</label>
-              <input type="text" required placeholder="Update your list" value={this.props.userInput}></input>
+              <input type="text" required placeholder="Update your list" value={this.props.userInput} name="newItem"></input>
               <button type="submit" className="submit-buttom m-1" >Add Item</button>
             </form>
           </div>  );
